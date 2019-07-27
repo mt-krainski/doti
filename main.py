b02c0f15f8723adf5accee23c5a257410d9adda4
Mateusz Krainski <mateusz.krainski@showmax.com>
2019-07-27 23:30:15 +0200

    Prototyping

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import numpy as np
 
 app = Flask(__name__)
 
+ROW_IDS = ["A", "B", "C"]
+ANSWER_RANGE_MIN = 1
+ANSWER_RANGE_MAX = 7
+
 
 @app.route("/")
-def hello_world():
-    return "Hello, World!"
+def table_form():
+    return render_template("table_form.html", row_ids=ROW_IDS)
+
+
+@app.route("/plot", methods=["post"])
+def make_plot():
+    print(request)
+    result_array = np.zeros(shape=(len(ROW_IDS), len(ROW_IDS)))
+    for i, col_id in enumerate(ROW_IDS):
+        for j, row_id in enumerate(ROW_IDS):
+            result_array[i, j] = int(
+                request.form[f"doti_{col_id}_{row_id}"] or "0"
+            )
+    print(result_array)
+    return "ok"
